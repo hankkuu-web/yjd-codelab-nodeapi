44a87f1db2d8088d7ea5ffaea64e1d9329259e04
jeonghwan-kim <ej88ej@gmail.com>
2017-01-11 17:43:09 +0900

    morgan 연동

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const logger = require('morgan');
 const bodyParser = require('body-parser')
 const app = express();
 const models = require('./models');
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logger('dev'));
 
 app.use('/users', require('./api/user'));
 
