b3c1ac749b96c3057ccfa6e0c61e34cd9c2028d7
jeonghwan-kim <ej88ej@gmail.com>
2017-01-11 11:52:28 +0900

    GET /users/:id

--- app/index.js
+++ app/index.js
@@ -13,7 +13,16 @@ let users = [{
   name: 'Chris'
 }];
 
-// 라우팅 설정
+app.get('/users/:id', (req, res)=> {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+
+  const user = users.filter(user => user.id.toString() === req.params.id)[0];
+  if (!user) return res.status(404).end();
+
+  res.json(user);
+});
+
 app.get('/users', (req, res) => {
   const limit = parseInt(req.query.limit, 10);
   if (Number.isNaN(limit)) return res.status(400).send();
--- app/index.spec.js
+++ app/index.spec.js
@@ -2,6 +2,13 @@ const should = require('should');
 const request = require('supertest');
 const app = require('./index');
 
+const assertUser = (user, id) => {
+  user.should.have.properties('id', 'name')
+  user.id.should.be.a.Number;
+  user.name.should.be.a.String;
+  if (id) user.id.should.be.equal(id);
+};
+
 describe('users', ()=> {
   describe('GET /users', ()=> {
     describe('success', ()=> {
@@ -19,7 +26,7 @@ describe('users', ()=> {
       });
       it('유저 객체를 담은 배열을 응답한다', ()=> {
         _res.body.should.be.instanceof(Array);
-        _res.body.forEach(user => user.should.have.properties('id', 'name'))
+        _res.body.forEach(user=> assertUser(user))
       });
       it('최대 limit 갯수만큼 응답한다', ()=> {
         _res.body.should.have.length(limit);
@@ -40,4 +47,35 @@ describe('users', ()=> {
       });
     });
   });
+  describe('GET /usres', ()=> {
+    describe('success', ()=> {
+      const id = 2;
+      let _res;
+      before(done => {
+        request(app)
+            .get(`/users/${id}`)
+            .expect(200)
+            .end((err, res) => {
+              if (err) throw err;
+              _res = res;
+              done();
+            })
+      });
+      it('id가 1인 유저 객체를 반환한다', ()=> assertUser(_res.body, id));
+    });
+    describe('error', ()=> {
+      it('id가 숫자가 아닐경우 400으로 응답한다', done=> {
+        request(app)
+            .get('/users/id')
+            .expect(400)
+            .end(done)
+      });
+      it('id로 유저를 찾을수 없을 경우 404로 응답한다', done=> {
+        request(app)
+            .get('/users/4')
+            .expect(404)
+            .end(done)
+      })
+    })
+  })
 });
