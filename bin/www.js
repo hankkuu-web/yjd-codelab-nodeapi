80cd44b70f729cbc5b2ddaf12616512a7d5347ee
jeonghwan-kim <ej88ej@gmail.com>
2017-01-14 18:49:12 +0900

    set port for eb

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,10 @@
 const app = require('../app');
 const db = require('./db');
 
+app.set('port', process.env.PORT || 3000);
+
+
 db.sync().then(_=> {
   console.log('Database sync')
-  app.listen(3000, () => console.log(`Run at http://localhost:3000`));
+  app.listen(app.get('port'), () => console.log(`Run at http://localhost:3000`));
 });
